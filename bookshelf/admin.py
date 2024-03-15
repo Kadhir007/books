60c165c85bf30794bf4e36cd6b031b7560aa9fcf
kadhir <kadhir@kadhir.com>
2024-03-15 12:58:43 +0530

    Add Book, Author and Genre models and register it to the Admin site

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from bookshelf.models import Book, Genre, Author
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+class Genre(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(Author, related_name='books' ,on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13, unique=True)
+    cover = models.ImageField(upload_to='images/', blank=True, null=True)
+    genre = models.ManyToManyField(Genre, related_name='books')
+
+    def __str__(self):
+        return self.title[:10]
